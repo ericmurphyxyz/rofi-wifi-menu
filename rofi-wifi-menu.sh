7689b9d15f41f5ffee0e4ac0107fe5fb15f36a15
ericmurphyxyz <eric@ericmurphy.xyz>
2021-12-24 13:43:26 +0700

    Refactor variable names

--- rofi-wifi-menu.sh
+++ rofi-wifi-menu.sh
@@ -3,11 +3,9 @@
 # Starts a scan of available broadcasting SSIDs
 # nmcli dev wifi rescan
 
-LIST=$(nmcli --fields "SSID,SECURITY" device wifi list | sed '/^--/d' | sed 1d)
+wifi_list=$(nmcli --fields "SSID,SECURITY" device wifi list | sed '/^--/d' | sed 1d)
 # Gives a list of known connections so we can parse it later
-KNOWNCON=$(nmcli -g NAME connection)
-
-CURRSSID=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
+saved_connections=$(nmcli -g NAME connection)
 
 connected=$(nmcli -fields WIFI g)
 if [[ "$connected" =~ "enabled" ]]; then
@@ -16,21 +14,20 @@ elif [[ "$connected" =~ "disabled" ]]; then
 	TOGGLE="Enable Wifi"
 fi
 
-
-CHENTRY=$(echo -e "$TOGGLE\n$LIST" | uniq -u | rofi -dmenu -p "Wi-Fi SSID: " )
-CHSSID=$(echo "$CHENTRY" | sed 's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
+chosen_network=$(echo -e "$TOGGLE\n$wifi_list" | uniq -u | rofi -dmenu -p "Wi-Fi SSID: " )
+chosen_id=$(echo "$chosen_network" | sed 's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
 
 	# If the connection is already in use, then this will still be able to get the SSID
-if [ "$CHSSID" = "*" ]; then
-	CHSSID=$(echo "$CHENTRY" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
+if [ "$chosen_id" = "*" ]; then
+	chosen_id=$(echo "$chosen_network" | sed  's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
 fi
 
 # Parses the list of preconfigured connections to see if it already contains the chosen SSID. This speeds up the connection process
-if [[ $(echo "$KNOWNCON" | grep -w "$CHSSID") = "$CHSSID" ]]; then
-	nmcli connection up id "$CHSSID"
+if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
+	nmcli connection up id "$chosen_id"
 else
-	if [[ "$CHENTRY" =~ "WPA2" ]] || [[ "$CHENTRY" =~ "WEP" ]]; then
-		WIFIPASS=$(echo "if connection is stored, hit enter" | rofi -dmenu -p "password: " )
+	if [[ "$chosen_network" =~ "WPA2" ]] || [[ "$chosen_network" =~ "WEP" ]]; then
+		wifi_password=$(echo "if connection is stored, hit enter" | rofi -dmenu -p "password: " )
 	fi
-	nmcli device wifi connect "$CHSSID" password "$WIFIPASS"
+	nmcli device wifi connect "$chosen_id" password "$wifi_password"
 fi
