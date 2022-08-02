f9442b01e0941b08f1ff55f343e79b580efda980
arthurbttf <arthurbttf@gmail.com>
2022-08-01 23:40:42 -0300

    Pede senha em wifi salvo caso mude

--- rofi-wifi-menu.sh
+++ rofi-wifi-menu.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-notify-send.py "Abrindo wifi..."
+notify-send "Getting list of available Wi-Fi networks..."
 # Get a list of available wifi connections and morph it into a nice-looking list
-wifi_list=$(nmcli --fields "IN-USE,SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
+wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
 
 connected=$(nmcli -fields WIFI g)
-if [[ "$connected" =~ "habilitado" ]]; then
-	toggle="睊  Desligar wifi"
-elif [[ "$connected" =~ "desabilitado" ]]; then
-	toggle="直  Ligar wifi"
+if [[ "$connected" =~ "enabled" ]]; then
+	toggle="睊  Disable Wi-Fi"
+elif [[ "$connected" =~ "disabled" ]]; then
+	toggle="直  Enable Wi-Fi"
 fi
 
 # Use rofi to select wifi network
@@ -18,29 +18,22 @@ chosen_id=$(echo "${chosen_network:3}" | xargs)
 
 if [ "$chosen_network" = "" ]; then
 	exit
-elif [ "$chosen_network" = "直  Ligar wifi" ]; then
+elif [ "$chosen_network" = "直  Enable Wi-Fi" ]; then
 	nmcli radio wifi on
-elif [ "$chosen_network" = "睊  Desligar wifi" ]; then
+elif [ "$chosen_network" = "睊  Disable Wi-Fi" ]; then
 	nmcli radio wifi off
 else
 	# Message to show when connection is activated successfully
-	success_message="Conectado a \"$chosen_id\"."
+	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
 	# Get saved connections
 	saved_connections=$(nmcli -g NAME connection)
-	# check saved connections
 	check_saved_connections=$(echo "$saved_connections" | grep -w "$chosen_id")
-
-	if [[ $check_saved_connections == *"Automático(a)"* ]]; then
-		chosen_id="$chosen_id Automático(a)"
-    	nmcli connection up id "$chosen_id" | grep "sucesso" || wifi_password=$(rofi -dmenu -p "Password: " ) && nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "sucesso" && notify-send.py "$success_message"
+	if [[ $check_saved_connections = "$chosen_id" ]]; then
+		nmcli connection up id "$chosen_id" | grep "successfully" || wifi_password=$(rofi -dmenu -p "Password: " ) && nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	else
-		if [[ $check_saved_connections = "$chosen_id" ]]; then
-			nmcli connection up id "$chosen_id" | grep "sucesso" || wifi_password=$(rofi -dmenu -p "Password: " ) && nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "sucesso" && notify-send.py "$success_message"
-		else
-			if [[ "$chosen_network" =~ "" ]]; then
-				wifi_password=$(rofi -dmenu -p "Password: " )
-			fi
-			nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "sucesso" && notify-send.py "Conexão estabelecida" "$success_message"
+		if [[ "$chosen_network" =~ "" ]]; then
+			wifi_password=$(rofi -dmenu -p "Password: " )
 		fi
+		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "$success_message"
 	fi
 fi
